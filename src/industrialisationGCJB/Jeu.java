18d6eac9eb402f9ddf8cf3121015729e427cb446
guigues85 <guillaume.charpentreau@gmail.com>
2014-09-22 16:07:08 +0200

    Retest pour Jérémy

--- src/industrialisationGCJB/Jeu.java
+++ src/industrialisationGCJB/Jeu.java
@@ -1,11 +1,41 @@
 package industrialisationGCJB;
 
 public class Jeu {
-	
+
 	private int JS1 = 0;
 	private int JS2 = 0;
+
+	public int getJS1() {
+		return JS1;
+	}
+	
+	public int getJS2() {
+		return JS2;
+	}
 	
 	public void score() {
-		System.out.println("Score J1 : " + JS1 + " - " + JS2 + " : Score J2");
+		System.out.println("" + JS1 + " - " + JS2 + "");
+	}
+
+	public void reset() {
+		JS1 = 0;
+		JS2 = 0;
+	}
+
+	public void pointJ1() {
+		if (JS1 == 0 || JS1 == 15) {
+			JS1 += 15;
+		} else if (JS1 == 30) {
+			JS1 += 10;
+		} else {
+			if (JS2 != 40) {
+				System.out.println("Victoire du Joueur 1 !");
+				reset();
+			} else {
+				System.out.println("Egalité !");
+				JS1 = 40;
+				JS2 = 40;
+			}
+		}
 	}
 }
--- src/industrialisationGCJB/Test.java
+++ src/industrialisationGCJB/Test.java
@@ -4,12 +4,44 @@ public class Test {
 
 	@Test
 	public void testInitialisationNouveauJeu() {
-		new Jeu();
+		Jeu jeu = new Jeu();
 	}
 
 	@Test 
 	public void testAfficherScoreDebutDePartie() {
-		Jeu jeu = new Jeu();
-		Assert.assertEquals("0-0", jeu.score()) ;
+		Assert.assertEquals("0-0", jeu.score());
+	}
+	
+	@Test 
+	public void testResetScore() {
+		Assert.assertEquals("0-0", jeu.reset());
+	}
+	
+	@Test 
+	public void testPointJ1() {
+		if (jeu.getJS1() == 0) {
+			Assert.assertEquals("15", jeu.pointJ1());
+		}
+	}
+	
+	@Test 
+	public void testPointJ1() {
+		if (jeu.getJS1() == 15) {
+			Assert.assertEquals("30", jeu.pointJ1());
+		}
+	}
+	
+	@Test 
+	public void testPointJ1() {
+		if (jeu.getJS1() == 30) {
+			Assert.assertEquals("40", jeu.pointJ1());
+		}
+	}
+	
+	@Test 
+	public void testPointJ1() {
+		if (jeu.getJS1() == 40) {
+			Assert.assertEquals("0", jeu.pointJ1());
+		}
 	}
 }
