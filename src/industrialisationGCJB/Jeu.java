a5cb8e71088ac612ef85cf210b2492e5a3b12e9c
jbouchet <imie@imie-dev-debian>
2014-09-22 14:18:57 +0200

    Tests classes Jeu.java et Test.java

---
+++ src/industrialisationGCJB/Jeu.java
@@ -0,0 +1,5 @@
+package industrialisationGCJB;
+
+public class Jeu {
+
+}
---
+++ src/industrialisationGCJB/Test.java
@@ -0,0 +1,9 @@
+package industrialisationGCJB;
+
+public class Test {
+
+	@org.junit.Test
+	public void testInitialisationNouveauJeu() {
+		new Jeu();
+	}
+}
