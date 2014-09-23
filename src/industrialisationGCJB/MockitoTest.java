b7f52a9d27d83e122edc4936f76cf999f27a384e
guigues85 <guillaume.charpentreau@gmail.com>
2014-09-23 11:12:45 +0200

    MockitoTest

---
+++ src/industrialisationGCJB/MockitoTest.java
@@ -0,0 +1,19 @@
+package industrialisationGCJB;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MockitoTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
