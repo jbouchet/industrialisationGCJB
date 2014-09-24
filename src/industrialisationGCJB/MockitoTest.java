9ff7bf0f8840cc5a1b533c53c6dec4552b842eba
jbouchet <imie@imie-dev-debian>
2014-09-24 10:14:05 +0200

    Décommenter

--- src/industrialisationGCJB/MockitoTest.java
+++ src/industrialisationGCJB/MockitoTest.java
@@ -9,30 +9,30 @@ import org.mockito.MockitoAnnotations;
 
 public class MockitoTest {
 
-//	@Before
-//	public void setUp() throws Exception {
-//		MockitoAnnotations.initMocks(this);
-//	}
-//	
-//	@Test 
-//    public void testSerialiserSaveMock()
-//    {
-//		Serialiser serialiser = Mockito.mock(Serialiser.class);
-//        Jeu jeu = new Jeu();
-//		Mockito.doNothing().when(serialiser).persist(Mockito.any(Jeu.class));
-//		jeu.setSeri(serialiser);  
-//		jeu.score();
-//        jeu.pointJ1();
-//        jeu.save();
-//    }
-//	
-//	@Test 
-//    public void testSerializerReset()
-//    {
-//        Jeu jeu = new Jeu(new Serialiser());
-//        jeu.score();
-//        jeu.pointJ1();
-//        jeu.save();
-//        jeu.reset();
-//    }
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test 
+    public void testSerialiserSaveMock()
+    {
+		Serialiser serialiser = Mockito.mock(Serialiser.class);
+        Jeu jeu = new Jeu();
+		Mockito.doNothing().when(serialiser).persist(Mockito.any(Jeu.class));
+		jeu.setSeri(serialiser);  
+		jeu.score();
+        jeu.pointJ1();
+        jeu.save();
+    }
+	
+	@Test 
+    public void testSerializerReset()
+    {
+        Jeu jeu = new Jeu(new Serialiser());
+        jeu.score();
+        jeu.pointJ1();
+        jeu.save();
+        jeu.reset();
+    }
 }
