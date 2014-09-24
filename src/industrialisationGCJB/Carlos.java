94640b0c073feb7a47ebff149bf096d2f89b7aea
guigues85 <guillaume.charpentreau@gmail.com>
2014-09-24 11:06:20 +0200

    chut!

--- src/industrialisationGCJB/Carlos.java
+++ src/industrialisationGCJB/Carlos.java
@@ -2,12 +2,12 @@ package industrialisationGCJB;
 
 public class Carlos {
 	
-	/*public static void main(String[] args) {
+	public static void main(String[] args) {
 		Serialiser serialiser = new Serialiser();
 		Jeu jeu = new Jeu(serialiser);
 		serialiser.read();
 		System.out.println(jeu.pointJ1());
 		serialiser.persist(jeu);
 		System.out.println(jeu.score());
-	}*/
+	}
 }
